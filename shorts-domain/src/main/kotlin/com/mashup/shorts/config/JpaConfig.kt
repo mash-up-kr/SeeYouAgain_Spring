aca5e49793b9c8e8e89b628db8c03fe69185f8fb
wjdrbd96 <wjdrbs966@naver.com>
2023-04-16 22:13:41 +0900

    [NO-ISSUE] jpa 설정 추가

---
+++ shorts-domain/src/main/kotlin/com/mashup/shorts/config/JpaConfig.kt
@@ -0,0 +1,8 @@
+package com.mashup.shorts.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@EnableJpaAuditing
+@Configuration
+class JpaConfig
