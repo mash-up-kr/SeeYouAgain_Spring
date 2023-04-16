ee4073853499ef75306beffebe6ef95398c0e733
wjdrbd96 <wjdrbs966@naver.com>
2023-04-16 22:13:41 +0900

    [NO-ISSUE] jpa 설정 추가

--- shorts-api/src/main/kotlin/com/mashup/shorts/ShortsApiApplication.kt
+++ shorts-api/src/main/kotlin/com/mashup/shorts/ShortsApiApplication.kt
@@ -7,5 +7,6 @@ import org.springframework.boot.runApplication
 class ShortsApiApplication
 
 fun main(args: Array<String>) {
+	System.setProperty("spring.config.location", "classpath:/domain-config/,classpath:/")
 	runApplication<ShortsApiApplication>(*args)
 }
