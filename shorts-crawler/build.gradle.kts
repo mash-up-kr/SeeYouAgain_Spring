5c5ec537c641d3fa14219228ef272fcd3577d5ce
K-Diger <kitten.diger@gmail.com>
2023-04-24 18:03:21 +0900

    [NO-ISSUE] : 크롤러 모듈 생성

---
+++ shorts-crawler/build.gradle.kts
@@ -0,0 +1,20 @@
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.mysql:mysql-connector-j")
+    implementation ("org.jsoup:jsoup:1.15.4")
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
+tasks.bootJar {
+    enabled = false
+}
+
+tasks.jar {
+    enabled = true
+}
