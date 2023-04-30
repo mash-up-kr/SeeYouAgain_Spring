06086f3dc4ea4106d68050e7edbfa9977f3a90ee
wjdrbd96 <wjdrbs966@naver.com>
2023-04-30 21:03:01 +0900

    feat: Exception 구조 및 Querydsl 설정 추가

--- shorts-domain/build.gradle.kts
+++ shorts-domain/build.gradle.kts
@@ -2,6 +2,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
+
+    // querydsl
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jpa")
 }
 
 allOpen {
