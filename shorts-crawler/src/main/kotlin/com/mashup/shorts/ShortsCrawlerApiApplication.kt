51759507dd324e39ae27dbfc124e56211cc481ec
K-Diger <kitten.diger@gmail.com>
2023-05-14 03:18:29 +0900

    feat : feat : 모든 카테고리(정치, 사회, 경제, 생활/문화, 세계, IT/과학)에 대한 제목, 내용, 날짜, 언론사 크롤링 추가

--- shorts-crawler/src/main/kotlin/com/mashup/shorts/ShortsCrawlerApiApplication.kt
+++ shorts-crawler/src/main/kotlin/com/mashup/shorts/ShortsCrawlerApiApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ShortsCrawlerApplication
+class ShortsCrawlerApiApplication
 
 fun main(args: Array<String>) {
-	runApplication<ShortsCrawlerApplication>(*args)
+	runApplication<ShortsCrawlerApiApplication>(*args)
 }
