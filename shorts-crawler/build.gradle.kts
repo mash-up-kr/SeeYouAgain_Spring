62f428c5272e7f9546a6484f20c4b8cf2a05d9f5
wjdrbd96 <wjdrbs966@naver.com>
2023-05-13 21:49:05 +0900

    fix: bootjar 버그 수정

--- shorts-crawler/build.gradle.kts
+++ shorts-crawler/build.gradle.kts
@@ -4,3 +4,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.jsoup:jsoup:1.15.4")
 }
+
+application {
+    mainClass.set("com.mashup.shorts.ShortsCrawlerApplication")
+}
