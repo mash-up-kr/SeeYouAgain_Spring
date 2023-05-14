391d1f907c557944a0925cec8cdd6413ec36a37e
K-Diger <kitten.diger@gmail.com>
2023-05-14 15:52:43 +0900

    fix : 도메인 모듈 내 javax 의존성 jakarta로 변경

--- shorts-domain/build.gradle.kts
+++ shorts-domain/build.gradle.kts
@@ -3,19 +3,14 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
 
-    // jasypt
-    api("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.3")
-
-    // querydsl
     implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
     kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
-    kapt("com.querydsl:querydsl-kotlin-codegen:5.0.0")
 }
 
 allOpen {
-    annotation("javax.persistence.Entity")
-    annotation("javax.persistence.MappedSuperclass")
-    annotation("javax.persistence.Embeddable")
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
 }
 
 tasks.bootJar {
