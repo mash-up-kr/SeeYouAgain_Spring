4d91b63c4a36a0e1f02aeae987c2106e7518323d
K-Diger <kitten.diger@gmail.com>
2023-06-01 21:04:54 +0900

    refactor : News 부생성자 추가

--- shorts-domain/src/main/kotlin/com/mashup/shorts/domain/news/News.kt
+++ shorts-domain/src/main/kotlin/com/mashup/shorts/domain/news/News.kt
@@ -36,4 +36,27 @@ class News(
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id", nullable = false)
     val category: Category,
-) : BaseEntity()
+) : BaseEntity() {
+
+    constructor(
+        extractedAllNews: List<List<Any>>,
+        titleIndex: Int,
+        contentIndex: Int,
+        imageLinkIndex: Int,
+        linkIndex: Int,
+        pressIndex: Int,
+        writtenDateTimeIndex: Int,
+        isHeadLineIndex: Int,
+        index: Int,
+        category: Category,
+    ) : this(
+        extractedAllNews[titleIndex][index] as? String ?: "",
+        extractedAllNews[contentIndex][index] as? String ?: "",
+        extractedAllNews[imageLinkIndex][index] as? String ?: "",
+        extractedAllNews[linkIndex][index] as? String ?: "",
+        extractedAllNews[pressIndex][index] as? String ?: "",
+        extractedAllNews[writtenDateTimeIndex][index] as? String ?: "",
+        if (extractedAllNews[isHeadLineIndex][index] as? Boolean == true) "HEADLINE" else "NORMAL",
+        category
+    )
+}
