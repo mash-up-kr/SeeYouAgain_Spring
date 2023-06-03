db1115fb09aeb5ced28691af1e2230a93035e0bb
sw-develop <psw00719@naver.com>
2023-06-03 16:50:14 +0900

    add: 카테고리명 Enum 추가

--- shorts-domain/src/main/kotlin/com/mashup/shorts/domain/category/Category.kt
+++ shorts-domain/src/main/kotlin/com/mashup/shorts/domain/category/Category.kt
@@ -3,11 +3,14 @@ package com.mashup.shorts.domain.category
 import com.mashup.shorts.domain.BaseEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.Table
 
 @Table(name = "category")
 @Entity
 class Category(
     @Column(name = "name", nullable = false, length = 15, unique = true)
-    val name: String,
+    @Enumerated(EnumType.STRING)
+    val name: CategoryName,
 ) : BaseEntity()
