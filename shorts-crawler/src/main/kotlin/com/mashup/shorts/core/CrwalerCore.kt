5652b83a47170bda3ee278b9b9c3b8f8016ce854
K-Diger <kitten.diger@gmail.com>
2023-06-05 19:25:33 +0900

    fix : 카드뉴스가 정상적으로 매핑되지 않게 저장되던 버그 수정

--- shorts-crawler/src/main/kotlin/com/mashup/shorts/core/CrwalerCore.kt
+++ shorts-crawler/src/main/kotlin/com/mashup/shorts/core/CrwalerCore.kt
@@ -9,6 +9,7 @@ import org.jsoup.select.Elements
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import com.mashup.shorts.common.util.Slf4j2KotlinLogging.log
+import com.mashup.shorts.domain.category.Category
 import com.mashup.shorts.domain.category.CategoryName
 import com.mashup.shorts.domain.category.CategoryRepository
 import com.mashup.shorts.domain.news.News
@@ -27,13 +28,6 @@ class CrawlerCore(
     fun executeCrawling() {
         // 특정 카테고리 순회
         for (categoryIndex: Int in urls.indices) {
-            log.info(LocalDateTime.now().toString() + " - " + urls[categoryIndex] + " - crawling start")
-            val doc = setup(urls[categoryIndex], categoryIndex)
-            val allHeadLineNewsLinks = extractAllHeadLineNewsLinks(doc)
-            val extractedAllNews = extractAllDetailNewsInHeadLine(allHeadLineNewsLinks, categoryIndex)
-            val numberOfHeadLine = extractedAllNews[RELATED_COUNT_INDEX].size
-            val newsCards = mutableListOf<NewsCard>()
-
             val category = when (categoryIndex) {
                 0 -> categoryRepository.findByName(CategoryName.POLITICS)
                 1 -> categoryRepository.findByName(CategoryName.ECONOMIC)
@@ -44,76 +38,41 @@ class CrawlerCore(
                 else -> null
             }
 
-            var relatedNewsCount = 0
+            log.info(LocalDateTime.now().toString() + " - " + urls[categoryIndex] + " - crawling start")
+            val doc = setup(urls[categoryIndex], categoryIndex)
+            val allNewsLinks = extractAllHeadLineNewsLinks(doc)
+            val newsCardBundle = extractNewsCardBundle(allNewsLinks, categoryIndex, category!!)
 
-            // 특정 카테고리의 헤드라인 갯수 만큼 순회
-            for (num: Int in 0 until numberOfHeadLine) {
-                val newsBundle = mutableListOf<News>()
-                val numberOfRelatedNews = extractedAllNews[RELATED_COUNT_INDEX][num].toString().toInt()
-
-                // 헤드라인을 기점으로 모든 카테고리 내의 기사들을 순회
-                for (index in relatedNewsCount until relatedNewsCount + numberOfRelatedNews) {
-                    if (index >= extractedAllNews[TITLE_INDEX].size) {
-                        break
-                    }
-                    val title = extractedAllNews[TITLE_INDEX][index].toString()
-                    val content = extractedAllNews[CONTENT_INDEX][index].toString()
-                    val imageLink = extractedAllNews[IMAGE_LINK_INDEX][index].toString()
-                        .substringAfter("data-src=\"")
-                        .substringBefore("\"")
-                    val link = extractedAllNews[LINK_INDEX][index].toString()
-                    val press = extractedAllNews[PRESS_INDEX][index].toString()
-                    val writtenDateTime = extractedAllNews[WRITTEN_DATETIME_INDEX][index].toString()
-                    val isHeadLine =
-                        if (extractedAllNews[IS_HEADLINE_INDEX][index] as? Boolean == true) HEADLINE else NORMAL
-
-                    if (category != null) {
-                        newsBundle.add(
-                            News(title, content, imageLink, link, press, writtenDateTime, isHeadLine, category)
-                        )
-                    }
-                }
-
-                relatedNewsCount += numberOfRelatedNews
-
-                for (news in newsBundle) {
+            for (newsCard in newsCardBundle) {
+                for (news in newsCard) {
                     newsRepository.save(news)
                 }
-
-                newsCards.add(
-                    NewsCard(
-                        category, newsBundle.map { it.id }.toString()
-                            .replace("[", "")
-                            .replace("]", ""),
-                        ""
-                    )
-                )
             }
 
-            for (newsCard in newsCards) {
-                if (newsCard.multipleNews?.split(",")?.isNotEmpty() == true) {
-                    val newsId = newsCard.multipleNews.toString()
-                        .substring(0, newsCard.multipleNews.toString().indexOf(","))
-                        .toLong()
-                    val headLineNewsContent =
-                        newsId.let { newsId.let { newsRepository.findById(it).get().content } }
-                    newsCard.insertKeyword(extractKeyword(headLineNewsContent))
-                    newsCardRepository.save(newsCard)
-                } else {
-                    val newsId = newsCard.multipleNews.toString().first().code.toLong()
-                    val headLineNewsContent =
-                        newsId.let { newsId.let { newsRepository.findById(it).get().content } }
-                    newsCard.insertKeyword(extractKeyword(headLineNewsContent))
-                    newsCardRepository.save(newsCard)
-                }
+            val createdNewsCardIds = mutableListOf<Long>()
+
+            for (newsCard in newsCardBundle) {
+                val persistenceNewsCard = NewsCard(
+                    category, filterSquareBracket(newsCard.map { it.id }.toString()),
+                    ""
+                )
+                createdNewsCardIds.add(newsCardRepository.save(persistenceNewsCard).id)
+            }
+
+            for (createdNewsCardId in createdNewsCardIds) {
+                val newsCard = newsCardRepository.findById(createdNewsCardId).get()
+                val newsIdInNewsCard = newsCard.multipleNews!!.split(", ").first().toLong()
+                val headLineNewsContent = newsRepository.findById(newsIdInNewsCard).get().content
+                newsCard.insertKeyword(extractKeyword(headLineNewsContent))
+                newsCardRepository.save(newsCard)
             }
 
             log.info("Take a break for 3 seconds to prevent request overload")
             Thread.sleep(3000)
-
         }
 
         log.info(LocalDateTime.now().toString() + " - " + "crawling done")
+
     }
 
     private fun extractKeyword(content: String): String {
@@ -166,20 +125,15 @@ class CrawlerCore(
             .distinct()
     }
 
-    private fun extractAllDetailNewsInHeadLine(
+    private fun extractNewsCardBundle(
         allHeadLineNewsLinks: List<String>,
         categorySeparator: Int,
-    ): List<List<Any>> {
-        val result = mutableListOf<MutableList<Any>>()
-        val detailHeadLineNewsTitles = mutableListOf<Any>()
-        val detailHeadLineContents = mutableListOf<Any>()
-        val detailHeadLineImageLinks = mutableListOf<Any>()
-        val detailHeadLineNewsLinks = mutableListOf<Any>()
-        val detailHeadLineNewsPresses = mutableListOf<Any>()
-        val detailHeadLineNewsWrittenDateTime = mutableListOf<Any>()
-        val detailHeadLineNewsIsHeadLine = mutableListOf<Any>()
-        val detailHeadLineNewsRelatedCount = mutableListOf<Any>()
+        category: Category,
+    ): List<MutableList<News>> {
+        val cardNewsBundle = mutableListOf<MutableList<News>>()
+        var cardNews = mutableListOf<News>()
 
+        // 헤드라인 갯수 만큼 반복
         for (link in allHeadLineNewsLinks) {
             var headLineFlag = true
             val moreDoc = Jsoup.connect(link).get()
@@ -188,57 +142,62 @@ class CrawlerCore(
                 .toString()
                 .split("</a>")
 
-            val relatedNewsCount = moreDoc
-                .getElementsByClass(RELATED_COUNT_CLASS_NAME)
-                .text()
-                .toInt()
+            val crawledLinks = mutableListOf<String>()
 
-            for (htmlLink in crawledHtmlLinks) {
+            // 특정 헤드라인에 속한 모든 기사를 순회한다.
+            loopInHeadLine@ for (htmlLink in crawledHtmlLinks) {
                 val detailLink = Jsoup.parse(htmlLink)
                     .select("a[href]")
                     .attr("href")
 
-                if (!detailHeadLineNewsLinks.contains(detailLink) && detailLink.isNotEmpty()) {
-                    detailHeadLineNewsLinks.add(detailLink)
-                    val detailDoc = Jsoup.connect(detailLink).get()
-                    val imageLink = detailDoc
-                        .getElementById(IMAGE_ID_NAME).toString()
-                    val title = detailDoc
-                        .getElementsByClass(TITLE_CLASS_NAME).text()
-                    val content = detailDoc
-                        .getElementsByClass(CONTENT_CLASS_NAME).addClass("#text").text()
-                    val press = detailDoc
-                        .getElementsByClass(PRESS_CLASS_NAME).text()
-                    val writtenDateTime = detailDoc
-                        .getElementsByClass(WRITTEN_DATETIME_CLASS_NAME).text()
+                if (detailLink.isNotEmpty()) {
+                    crawledLinks.add(detailLink)
+                    Thread.sleep(100)
+                    val detailDoc = Jsoup.connect(detailLink)
+                        .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0")
+                        .get()
+                    val title = detailDoc.getElementsByClass(TITLE_CLASS_NAME).text()
 
-                    if (!detailHeadLineNewsTitles.contains(title)) {
-                        detailHeadLineNewsTitles.add(title)
-                        detailHeadLineContents.add(content)
-                        detailHeadLineImageLinks.add(imageLink)
-                        detailHeadLineNewsPresses.add(press)
-                        detailHeadLineNewsWrittenDateTime.add(writtenDateTime)
-
-                        if (headLineFlag) {
-                            detailHeadLineNewsIsHeadLine.add(true)
-                            headLineFlag = false
-                        } else {
-                            detailHeadLineNewsIsHeadLine.add(false)
-                        }
+                    if (crawledLinks.contains(title)) {
+                        continue@loopInHeadLine
                     }
+
+                    val content = detailDoc
+                        .getElementsByClass(CONTENT_CLASS_NAME).addClass("#text")
+                        .text()
+                    val imageLink = detailDoc.getElementById(IMAGE_ID_NAME).toString()
+                    val press = detailDoc.getElementsByClass(PRESS_CLASS_NAME).text()
+                    val writtenDateTime = detailDoc.getElementsByClass(WRITTEN_DATETIME_CLASS_NAME).text()
+
+                    cardNews.add(
+                        News(
+                            title,
+                            content,
+                            imageLink,
+                            detailLink,
+                            press,
+                            writtenDateTime,
+                            if (headLineFlag) {
+                                HEADLINE
+                            } else {
+                                NORMAL
+                            },
+                            category
+                        )
+                    )
+                    headLineFlag = false
                 }
             }
-            detailHeadLineNewsRelatedCount.add(relatedNewsCount)
-            result.add(detailHeadLineNewsTitles)
-            result.add(detailHeadLineContents)
-            result.add(detailHeadLineImageLinks)
-            result.add(detailHeadLineNewsLinks)
-            result.add(detailHeadLineNewsPresses)
-            result.add(detailHeadLineNewsWrittenDateTime)
-            result.add(detailHeadLineNewsIsHeadLine)
-            result.add(detailHeadLineNewsRelatedCount)
+            cardNewsBundle.add(cardNews)
+            cardNews = mutableListOf()
         }
-        return result
+        return cardNewsBundle
+    }
+
+    private fun filterSquareBracket(target: String): String {
+        return target
+            .replace("[", "")
+            .replace("]", "")
     }
 
     companion object {
@@ -296,16 +255,6 @@ class CrawlerCore(
         private const val IMAGE_ID_NAME = "img1"
         private const val PRESS_CLASS_NAME = "media_end_linked_more_point"
         private const val WRITTEN_DATETIME_CLASS_NAME = "media_end_head_info_datestamp_time _ARTICLE_DATE_TIME"
-        private const val RELATED_COUNT_CLASS_NAME = "cluster_banner_count_icon_num"
-
-        private const val TITLE_INDEX = 0
-        private const val CONTENT_INDEX = 1
-        private const val IMAGE_LINK_INDEX = 2
-        private const val LINK_INDEX = 3
-        private const val PRESS_INDEX = 4
-        private const val WRITTEN_DATETIME_INDEX = 5
-        private const val IS_HEADLINE_INDEX = 6
-        private const val RELATED_COUNT_INDEX = 7
 
         private const val HEADLINE = "HEADLINE"
         private const val NORMAL = "NORMAL"
