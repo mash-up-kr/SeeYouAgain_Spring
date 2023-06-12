c8dd7f2fbac8581747636f5a0f14b70240377e20
wjdrbd96 <wjdrbs966@naver.com>
2023-06-12 23:14:20 +0900

    feat: userId Header 에서 가져올 수 있도록 AOP 추가

--- shorts-api/src/main/kotlin/com/mashup/shorts/domain/member/membernewscard/MemberNewsCardCreateApi.kt
+++ shorts-api/src/main/kotlin/com/mashup/shorts/domain/member/membernewscard/MemberNewsCardCreateApi.kt
@@ -3,10 +3,11 @@ package com.mashup.shorts.domain.member.membernewscard
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import com.mashup.shorts.common.response.ApiResponse
+import com.mashup.shorts.config.aop.Auth
+import com.mashup.shorts.config.aop.AuthContext
 import com.mashup.shorts.domain.member.membernewscard.dto.MemberNewsCardCreateRequest
 
 @RequestMapping("/v1/member/news-card")
@@ -15,11 +16,11 @@ class MemberNewsCardCreateApi(
     private val memberNewsCardCreate: MemberNewsCardCreate
 ) {
 
+    @Auth
     @PostMapping
-    fun createMemberNewsCard(@RequestHeader(value = "Authorization") uniqueId: String,
-                             @RequestBody memberNewsCardCreateRequest: MemberNewsCardCreateRequest
-    ): ApiResponse<Void> {
-        memberNewsCardCreate.createMemberNewsCard(uniqueId, memberNewsCardCreateRequest.newsCardId)
+    fun createMemberNewsCard(@RequestBody memberNewsCardCreateRequest: MemberNewsCardCreateRequest): ApiResponse<Void> {
+        val memberUniqueId = AuthContext.getMemberId()
+        memberNewsCardCreate.createMemberNewsCard(memberUniqueId, memberNewsCardCreateRequest.newsCardId)
         return ApiResponse.success(HttpStatus.CREATED)
     }
 }
