84b69a9a566264509b7df608e885a31da5baca16
sw-develop <psw00719@naver.com>
2023-06-13 00:52:40 +0900

    feat: 뉴스 저장 API 추가 (오래 간직할 뉴스)

---
+++ shorts-api/src/main/kotlin/com/mashup/shorts/domain/member/membernews/MemberNewsCreateApi.kt
@@ -0,0 +1,25 @@
+package com.mashup.shorts.domain.member.membernews
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import com.mashup.shorts.common.response.ApiResponse
+import com.mashup.shorts.domain.member.membernews.dto.MemberNewsCreateRequest
+
+@RequestMapping("/v1/member/news")
+@RestController
+class MemberNewsCreateApi(
+    private val memberNewsCreate: MemberNewsCreate
+) {
+
+    @PostMapping
+    fun createMemberNews(@RequestHeader("Authorization") uniqueId: String,
+                         @RequestBody memberNewsCreateRequest: MemberNewsCreateRequest
+    ): ApiResponse<Void> {
+        memberNewsCreate.createMemberNews(uniqueId, memberNewsCreateRequest.newsId)
+        return ApiResponse.success(HttpStatus.CREATED)
+    }
+}
---
+++ shorts-api/src/test/kotlin/com/mashup/shorts/api/restdocs/member/news/MemberNewsCreateApiTest.kt
@@ -0,0 +1,55 @@
+package com.mashup.shorts.api.restdocs.member.news
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
+import org.springframework.restdocs.headers.HeaderDocumentation
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders
+import org.springframework.restdocs.payload.JsonFieldType
+import org.springframework.restdocs.payload.PayloadDocumentation
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import com.mashup.shorts.api.ApiDocsTestBase
+import com.mashup.shorts.domain.member.membernews.MemberNewsCreate
+import com.mashup.shorts.domain.member.membernews.MemberNewsCreateApi
+import com.mashup.shorts.domain.member.membernews.dto.MemberNewsCreateRequest
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+
+@WebMvcTest(MemberNewsCreateApi::class)
+class MemberNewsCreateApiTest : ApiDocsTestBase() {
+
+    @MockkBean
+    private lateinit var memberNewsCreate: MemberNewsCreate
+
+    @Test
+    fun `오래 간직할 뉴스 추가`() {
+        every { memberNewsCreate.createMemberNews(any(), any()) } returns(Unit)
+
+        val requestBody = MemberNewsCreateRequest(newsId = 1L)
+
+        mockMvc.perform(
+            RestDocumentationRequestBuilders.post("/v1/member/news")
+                .header("Authorization", "test-user")
+                .content(objectMapper.writeValueAsString(requestBody))
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andDo(
+                MockMvcRestDocumentation.document(
+                    "오래 간직할 뉴스 추가",
+                    HeaderDocumentation.requestHeaders(
+                        HeaderDocumentation.headerWithName("Authorization").description("사용자 식별자 id")
+                    ),
+                    PayloadDocumentation.requestFields(
+                        PayloadDocumentation.fieldWithPath("newsId").type(JsonFieldType.NUMBER).description("뉴스 id")
+                    ),
+                    PayloadDocumentation.responseFields(
+                        PayloadDocumentation.fieldWithPath("status").type(JsonFieldType.NUMBER).description("API HTTP Status 값"),
+                        PayloadDocumentation.fieldWithPath("result").type(JsonFieldType.NULL).description("반환 결과 없음")
+                    )
+                )
+            )
+    }
+}
