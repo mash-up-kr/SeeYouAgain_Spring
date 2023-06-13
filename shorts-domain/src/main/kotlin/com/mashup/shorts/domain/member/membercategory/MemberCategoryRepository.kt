790646238de4435fad27cb79db34da3285784fcc
K-Diger <kitten.diger@gmail.com>
2023-06-13 20:28:16 +0900

    feat : 메인 페이지 조회 기능 추가

--- shorts-domain/src/main/kotlin/com/mashup/shorts/domain/member/membercategory/MemberCategoryRepository.kt
+++ shorts-domain/src/main/kotlin/com/mashup/shorts/domain/member/membercategory/MemberCategoryRepository.kt
@@ -2,6 +2,9 @@ package com.mashup.shorts.domain.member.membercategory
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import com.mashup.shorts.domain.member.Member
 
 @Repository
-interface MemberCategoryRepository : JpaRepository<MemberCategory, Long>
+interface MemberCategoryRepository : JpaRepository<MemberCategory, Long> {
+    fun findByMember(member: Member): List<MemberCategory>?
+}
--- shorts-domain/src/main/kotlin/com/mashup/shorts/domain/newscard/NewsCardRepository.kt
+++ shorts-domain/src/main/kotlin/com/mashup/shorts/domain/newscard/NewsCardRepository.kt
@@ -1,7 +1,42 @@
 package com.mashup.shorts.domain.newscard
 
+import java.time.LocalDate
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
+import com.mashup.shorts.domain.category.Category
 
 @Repository
-interface NewsCardRepository : JpaRepository<NewsCard, Long>
+interface NewsCardRepository : JpaRepository<NewsCard, Long> {
+
+    @Query(
+        "SELECT * FROM news_card " +
+            "WHERE id > :cursorId " +
+            "AND created_at BETWEEN :targetDate AND (:targetDate + INTERVAL + :targetHour HOUR) " +
+            "ORDER BY id ASC " +
+            "LIMIT :size", nativeQuery = true
+    )
+    fun findNewsCardsByTargetTimeAndAndMemberCategoryAndCursorId(
+        @Param("targetDate") targetDate: LocalDate,
+        @Param("targetHour") targetHour: Int,
+        @Param("cursorId") cursorId: Long,
+        @Param("size") size: Int,
+    ): List<NewsCard>
+
+    @Query(
+        "SELECT * FROM news_card " +
+            "WHERE id > :cursorId " +
+            "AND created_at BETWEEN :targetDate AND (:targetDate + INTERVAL + :targetHour HOUR) " +
+            "AND category_id in :categories " +
+            "ORDER BY id ASC " +
+            "LIMIT :size", nativeQuery = true
+    )
+    fun findNewsCardsByTargetTimeAndAndMemberCategoryAndCursorIdAndCategory(
+        @Param("targetDate") targetDate: LocalDate,
+        @Param("targetHour") targetHour: Int,
+        @Param("cursorId") cursorId: Long,
+        @Param("size") size: Int,
+        @Param("categories") categories: List<Long>,
+    ): List<NewsCard>
+}
