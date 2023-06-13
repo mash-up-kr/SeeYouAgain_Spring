f1bd0d5ce3fd6a05103142303c7de38c6fa501b7
K-Diger <kitten.diger@gmail.com>
2023-06-14 00:20:47 +0900

    docs : 임시 스웨거 추가

--- shorts-api/src/main/kotlin/com/mashup/shorts/domain/member/membernewscard/dto/MemberNewsCardRequset.kt
+++ shorts-api/src/main/kotlin/com/mashup/shorts/domain/member/membernewscard/dto/MemberNewsCardRequset.kt
@@ -1,6 +1,12 @@
 package com.mashup.shorts.domain.member.membernewscard.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class MemberNewsCardClearRequest(
+
+    @Schema(description = "멤버 Id", example = "1")
     val memberId: Long,
+
+    @Schema(description = "뉴스카드 Id", example = "1")
     val newsCardId: Long,
 )
--- shorts-api/src/main/kotlin/com/mashup/shorts/domain/memberCategory/CategoryCreateBulkRequest.kt
+++ shorts-api/src/main/kotlin/com/mashup/shorts/domain/memberCategory/CategoryCreateBulkRequest.kt
@@ -1,6 +1,7 @@
 package com.mashup.shorts.domain.memberCategory
 
 import com.mashup.shorts.domain.category.CategoryName
+import io.swagger.v3.oas.annotations.media.Schema
 
 /**
  * CategoryCreateBulkRequest
@@ -10,5 +11,6 @@ import com.mashup.shorts.domain.category.CategoryName
  * @since 2023. 06. 11.
  */
 data class CategoryCreateBulkRequest(
+    @Schema(description = "추가할 카테고리 이름", example = "POLITICS, ECONOMY ...")
     val categoryNames: List<CategoryName>
 )
