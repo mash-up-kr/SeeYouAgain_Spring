162ee62f57337aec198695ef338c839328d56f5b
K-Diger <kitten.diger@gmail.com>
2023-06-14 17:34:56 +0900

    fix : @EnableScheduling 추가

--- shorts-crawler/src/main/kotlin/com/mashup/shorts/ShortsCrawlerApplication.kt
+++ shorts-crawler/src/main/kotlin/com/mashup/shorts/ShortsCrawlerApplication.kt
@@ -2,10 +2,12 @@ package com.mashup.shorts
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @SpringBootApplication
 class ShortsCrawlerApplication
 
 fun main(args: Array<String>) {
-	runApplication<ShortsCrawlerApplication>(*args)
+    runApplication<ShortsCrawlerApplication>(*args)
 }
