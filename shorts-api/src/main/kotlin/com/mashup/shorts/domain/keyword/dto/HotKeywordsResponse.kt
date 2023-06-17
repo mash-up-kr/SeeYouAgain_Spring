ca86ddb259ad973c575e4df4d82f23d1f90f502f
sw-develop <psw00719@naver.com>
2023-06-17 17:01:03 +0900

    add: 핫 키워드 조회 API 추가

---
+++ shorts-api/src/main/kotlin/com/mashup/shorts/domain/keyword/dto/HotKeywordsResponse.kt
@@ -0,0 +1,13 @@
+package com.mashup.shorts.domain.keyword.dto
+
+data class HotKeywordsResponse(
+    val createdAt: String,
+    val ranking: List<String>
+) {
+
+    companion object {
+        fun of(createdAt: String, ranking: List<String>): HotKeywordsResponse {
+            return HotKeywordsResponse(createdAt = createdAt, ranking = ranking)
+        }
+    }
+}
