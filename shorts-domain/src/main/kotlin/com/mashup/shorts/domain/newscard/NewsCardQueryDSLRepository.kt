4efc9f0c8f08d5a05fe0e033e52d705e5593a8af
K-Diger <kitten.diger@gmail.com>
2023-06-23 00:48:27 +0900

    fix : 홈 조회 시 targetDateTime 을 조건절 하지 않던 버그 수정

--- shorts-domain/src/main/kotlin/com/mashup/shorts/domain/newscard/NewsCardQueryDSLRepository.kt
+++ shorts-domain/src/main/kotlin/com/mashup/shorts/domain/newscard/NewsCardQueryDSLRepository.kt
@@ -1,10 +1,14 @@
 package com.mashup.shorts.domain.newscard
 
+import java.time.LocalDateTime
+
 interface NewsCardQueryDSLRepository {
 
     fun findNewsCardsByMemberFilteredNewsIdsAndCursorId(
         filteredNewsIds: List<Long>,
         cursorId: Long,
+        startDateTime: LocalDateTime,
+        endDateTime: LocalDateTime,
         categories: List<Long>,
         size: Int,
     ): List<NewsCard>
