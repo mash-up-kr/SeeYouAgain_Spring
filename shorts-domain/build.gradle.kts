2e858786949e01de74758cf8de1788e0fba132e4
K-Diger <kitten.diger@gmail.com>
2023-06-26 19:48:10 +0900

    chore : flyway dependency 제거

--- shorts-domain/build.gradle.kts
+++ shorts-domain/build.gradle.kts
@@ -11,9 +11,6 @@ dependencies {
     kapt ("com.querydsl:querydsl-apt:5.0.0:jakarta")
     kapt ("jakarta.annotation:jakarta.annotation-api")
     kapt ("jakarta.persistence:jakarta.persistence-api")
-
-    // Flyway
-    implementation("org.flywaydb:flyway-mysql")
 }
 
 allOpen {
