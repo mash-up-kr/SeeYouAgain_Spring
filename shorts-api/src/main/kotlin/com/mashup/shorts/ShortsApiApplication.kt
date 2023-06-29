fdd08a71791b271ad8be1d8641e5a0d89754a79d
K-Diger <kitten.diger@gmail.com>
2023-06-29 21:06:55 +0900

    feat : 0시 오늘의 숏스 초기화 기능 추가

--- shorts-api/src/main/kotlin/com/mashup/shorts/ShortsApiApplication.kt
+++ shorts-api/src/main/kotlin/com/mashup/shorts/ShortsApiApplication.kt
@@ -2,7 +2,9 @@ package com.mashup.shorts
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @SpringBootApplication
 class ShortsApiApplication
 
