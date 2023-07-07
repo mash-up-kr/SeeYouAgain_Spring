45a51f26f3e9cbb9ab4423715112d81745d83df0
K-Diger <kitten.diger@gmail.com>
2023-07-07 16:55:55 +0900

    chore : 요청 LocalDateTime으로 StartDateTime과 EndDateTime을 추출하는 로직 분리

---
+++ shorts-domain/src/main/kotlin/com/mashup/shorts/common/util/StartEndDateTimeExtractor.kt
@@ -0,0 +1,23 @@
+package com.mashup.shorts.common.util
+
+import java.time.LocalDateTime
+
+object StartEndDateTimeExtractor {
+    fun extractStarDateTimeAndEndDateTime(
+        targetDateTime: LocalDateTime,
+    ): Pair<LocalDateTime, LocalDateTime> {
+        val startDateTime = targetDateTime
+            .withHour(targetDateTime.hour)
+            .withMinute(0)
+            .withSecond(0)
+            .withNano(0)
+
+        val endDateTime = targetDateTime
+            .withHour(targetDateTime.hour)
+            .withMinute(59)
+            .withSecond(59)
+            .withNano(59)
+
+        return Pair(startDateTime, endDateTime)
+    }
+}
