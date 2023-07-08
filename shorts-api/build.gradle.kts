7f6b3f05f4aa3f88b60f189409f61c353a4a4578
wjdrbd96 <wjdrbs966@naver.com>
2023-07-08 21:16:04 +0900

    feat: build gradle jib 설정 추가

--- shorts-api/build.gradle.kts
+++ shorts-api/build.gradle.kts
@@ -47,7 +47,7 @@ application {
 
 jib {
     from {
-        image = "adoptopenjdk/openjdk17:alpine-jre"
+        image = "eclipse-temurin:11-jre-focal"
     }
     to {
         image = System.getProperty("image", "shorts/shorts-api")
