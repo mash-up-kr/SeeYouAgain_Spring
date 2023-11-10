6369f4d328a4cf1247764eca37a55782280b4637
Diger <kitten.diger@gmail.com>
2023-11-10 13:03:32 +0900

    docs : Modify Server Address

--- shorts-domain/src/main/kotlin/com/mashup/shorts/common/response/ExceptionResponse.kt
+++ shorts-domain/src/main/kotlin/com/mashup/shorts/common/response/ExceptionResponse.kt
@@ -2,13 +2,6 @@ package com.mashup.shorts.common.response
 
 import com.mashup.shorts.common.exception.ShortsErrorCode
 
-/**
- * ExceptionResponse
- *
- * @author JungGyun.Choi
- * @version 1.0.0
- * @since 2023. 04. 30.
- */
 data class ErrorResponse(
     val status: Int,
     val error: Error
