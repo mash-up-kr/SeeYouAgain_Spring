311851b6dceec5f30f3fe624a483a9a2d71994e6
Diger <kitten.diger@gmail.com>
2024-02-27 19:27:10 +0900

    refactor : remove not used dependency

--- shorts-crawler/build.gradle.kts
+++ shorts-crawler/build.gradle.kts
@@ -8,10 +8,6 @@ dependencies {
     // JSoup Dependency
     implementation("org.jsoup:jsoup:1.15.4")
 
-    // Lucene Dependency
-    implementation("org.apache.lucene:lucene-core:8.4.1")
-    implementation("org.apache.lucene:lucene-analyzers-nori:8.4.1")
-
     // Komoran Dependency
     implementation("com.github.shin285:KOMORAN:3.3.9")
 
