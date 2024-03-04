9925680128d1463af55f31caadc214ceb31c19dd
Diger <kitten.diger@gmail.com>
2024-03-04 18:47:28 +0900

    refactor : Add Elements in StopWord

--- shorts-crawler/src/main/kotlin/com/mashup/shorts/leagcy/keywordextractor/StopWord.kt
+++
@@ -1,6 +0,0 @@
-package com.mashup.shorts.leagcy.keywordextractor
-
-@Deprecated("Deprecated By Changed DOM")
-val stopWords = setOf(
-    "은", "는", "이", "가", "을", "를", "과", "와", "에서", "으로", "에게", "으로부터", "에", "의"
-)
