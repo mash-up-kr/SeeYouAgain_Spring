1a828e98cdd2672200244da0656f375441f39c1e
Diger <kitten.diger@gmail.com>
2024-03-20 02:45:49 +0900

    Update Mysql Connector Version

--- shorts-domain/build.gradle.kts
+++ shorts-domain/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     api(project(":shorts-common"))
 
     api("org.springframework.boot:spring-boot-starter-data-jpa")
-    runtimeOnly("com.mysql:mysql-connector-j")
+    api("com.mysql:mysql-connector-j:8.0.33")
 
     // jasypt
     api("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.3")
